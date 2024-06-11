bc25e7f7a8104facd5dfa944e57c6c3723993cf1
cdrmack <git.pynvt@passmail.net>
2024-06-11 22:52:08 +0200

    draw box using ncurses

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate ncurses;
+use ncurses::*;
+
 #[derive(Debug, PartialEq)]
 enum EntryState {
     Todo,
@@ -13,7 +16,10 @@ struct Entry {
 
 impl Entry {
     fn new(description: String) -> Self {
-	Entry { description, state: EntryState::Todo }
+	Entry {
+	    description,
+	    state: EntryState::Todo,
+	}
     }
 
     fn change_state(&mut self, new_state: EntryState) {
@@ -23,18 +29,30 @@ impl Entry {
     }
 }
 
+const HELP_WIDTH: i32 = 40;
+const HELP_HEIGHT: i32 = 6;
+
 fn main() {
-    let mut todo_list = vec![];
-    todo_list.push(Entry::new(String::from("foo")));
-    todo_list.push(Entry::new(String::from("bar")));
+    let root = initscr();
 
-    for entry in todo_list.iter() {
-	println!("Entry = {:?}", entry);
-    }
+    let mut max_x: i32 = 0;
+    let mut max_y: i32 = 0;
 
-    todo_list[1].change_state(EntryState::InProgress);
+    let _ = getmaxyx(root, &mut max_y, &mut max_x);
 
-    for entry in todo_list.iter() {
-	println!("Entry = {:?}", entry);
-    }
+    let w = newwin(
+	HELP_HEIGHT,
+	HELP_WIDTH,
+	max_y - HELP_HEIGHT,
+	(max_x / 2) - (HELP_WIDTH / 2),
+    );
+
+    box_(w, 0, 0);
+    mvwprintw(w, 1, 1, "HOWTO:");
+    mvwprintw(w, 2, 1, "FOO");
+    mvwprintw(w, 3, 1, "FOO");
+    mvwprintw(w, 4, 1, "FOO");
+    wgetch(w);
+
+    endwin();
 }
