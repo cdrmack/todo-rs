c5dc5040c9f193e7d05921f0708dd52d565365a1
cdrmack <git.pynvt@passmail.net>
2024-06-16 11:02:56 +0200

    allow to jump between windows

--- src/main.rs
+++ src/main.rs
@@ -16,23 +16,24 @@ struct Entry {
 
 impl Entry {
     fn new(description: String) -> Self {
-        Entry {
-            description,
-            state: EntryState::Todo,
-        }
+	Entry {
+	    description,
+	    state: EntryState::Todo,
+	}
     }
 
     fn change_state(&mut self, new_state: EntryState) {
-        if self.state != new_state {
-            self.state = new_state;
-        }
+	if self.state != new_state {
+	    self.state = new_state;
+	}
     }
 
     fn get_description(&self) -> &str {
-        &self.description
+	&self.description
     }
 }
 
+#[derive(PartialEq)]
 enum Window {
     CurrentTasks,
     ArchivedTasks,
@@ -43,56 +44,72 @@ struct Todo {
     pub current_tasks: Vec<Entry>,
     archived_tasks: Vec<Entry>,
     pub cursor: usize,
-    active_window: Window,
+    pub active_window: Window,
 }
 
 impl Todo {
     fn add_task(&mut self, new_entry: Entry) {
-        self.current_tasks.push(new_entry);
+	self.current_tasks.push(new_entry);
     }
 
     fn cursor_up(&mut self) {
-        if (self.cursor > 0) {
-            self.cursor -= 1;
-        }
+	if self.cursor > 0 {
+	    self.cursor -= 1;
+	}
     }
 
     fn cursor_down(&mut self) {
-        match self.active_window {
-            Window::CurrentTasks => {
-                if (self.cursor < self.current_tasks.len() - 1) {
-                    self.cursor += 1;
-                }
-            }
-            Window::ArchivedTasks => {
-                if (self.cursor < self.archived_tasks.len() - 1) {
-                    self.cursor += 1;
-                }
-            }
-            Window::Help => {}
-        }
+	match self.active_window {
+	    Window::CurrentTasks => {
+		if self.cursor < self.current_tasks.len() - 1 {
+		    self.cursor += 1;
+		}
+	    }
+	    Window::ArchivedTasks => {
+		if self.cursor < self.archived_tasks.len() - 1 {
+		    self.cursor += 1;
+		}
+	    }
+	    Window::Help => {}
+	}
+    }
+
+    fn cursor_change_window(&mut self) {
+	match self.active_window {
+	    Window::CurrentTasks => self.active_window = Window::ArchivedTasks,
+	    Window::ArchivedTasks => self.active_window = Window::CurrentTasks,
+	    _ => {},
+	}
+	self.cursor = 0;
     }
 }
 
-const HELP_WIDTH: i32 = 40;
-const HELP_HEIGHT: i32 = 5;
-
 fn refresh_current(current: WINDOW, todo: &Todo) {
     for (i, item) in todo.current_tasks.iter().enumerate() {
-        if (i == todo.cursor) {
-            wattron(current, A_BOLD | A_UNDERLINE);
-        } else {
-            wattroff(current, A_BOLD | A_UNDERLINE);
-        }
-        let _ = mvwprintw(current, i as i32 + 1, 1, item.get_description());
+	if todo.active_window == Window::CurrentTasks && i == todo.cursor {
+	    wattron(current, A_BOLD | A_UNDERLINE);
+	} else {
+	    wattroff(current, A_BOLD | A_UNDERLINE);
+	}
+	let _ = mvwprintw(current, i as i32 + 1, 1, item.get_description());
     }
     wrefresh(current);
 }
 
 fn refresh_archived(archived: WINDOW, todo: &Todo) {
-    // TODO
+    for (i, item) in todo.archived_tasks.iter().enumerate() {
+	if todo.active_window == Window::ArchivedTasks && i == todo.cursor {
+	    wattron(archived, A_BOLD | A_UNDERLINE);
+	} else {
+	    wattroff(archived, A_BOLD | A_UNDERLINE);
+	}
+	let _ = mvwprintw(archived, i as i32 + 1, 1, item.get_description());
+    }
     wrefresh(archived);
 }
+
+const HELP_WIDTH: i32 = 40;
+const HELP_HEIGHT: i32 = 5;
 
 fn main() {
     let root = initscr();
@@ -105,16 +122,16 @@ fn main() {
     let _ = getmaxyx(root, &mut max_y, &mut max_x);
 
     let help = newwin(
-        HELP_HEIGHT,
-        HELP_WIDTH,
-        max_y - HELP_HEIGHT,
-        (max_x / 2) - (HELP_WIDTH / 2),
+	HELP_HEIGHT,
+	HELP_WIDTH,
+	max_y - HELP_HEIGHT,
+	(max_x / 2) - (HELP_WIDTH / 2),
     );
     box_(help, 0, 0);
     let _ = mvwprintw(help, 0, 0, "HELP");
     let _ = mvwprintw(help, 1, 1, "up/down - navigate");
-    let _ = mvwprintw(help, 2, 1, "q - quit");
-    let _ = mvwaddstr(help, 3, 1, "ASD");
+    let _ = mvwprintw(help, 2, 1, "TAB - change window");
+    let _ = mvwaddstr(help, 3, 1, "q - quit");
     wrefresh(help);
 
     let current_tasks = newwin(max_y - HELP_HEIGHT, max_x / 2, 0, 0);
@@ -126,30 +143,31 @@ fn main() {
     let _ = mvwprintw(archived_tasks, 0, 0, "ARCHIVED");
 
     // tmp data
+    let mut tmp1 = Entry::new("fourth task".to_string());
+    tmp1.change_state(EntryState::Done);
+    let mut tmp2 = Entry::new("fifth task".to_string());
+    tmp2.change_state(EntryState::Done);
     let mut todo = Todo {
-        current_tasks: vec![],
-        archived_tasks: vec![],
-        cursor: 0,
-        active_window: Window::CurrentTasks,
+	current_tasks: vec![],
+	archived_tasks: vec![tmp1, tmp2],
+	cursor: 0,
+	active_window: Window::CurrentTasks,
     };
     todo.add_task(Entry::new("first task".to_string()));
     todo.add_task(Entry::new("second task".to_string()));
     todo.add_task(Entry::new("third task".to_string()));
 
     loop {
-        // TODO: break condition
-        refresh_current(current_tasks, &todo);
-        refresh_archived(archived_tasks, &todo);
-        let ch = getch();
-        match ch {
-            KEY_UP => todo.cursor_up(),
-            KEY_DOWN => todo.cursor_down(),
-            // 113 is for `q`
-            113 => {
-                break;
-            }
-            _ => {}
-        }
+	refresh_current(current_tasks, &todo);
+	refresh_archived(archived_tasks, &todo);
+	let ch = getch();
+	match ch {
+	    KEY_UP => todo.cursor_up(),
+	    KEY_DOWN => todo.cursor_down(),
+	    9 => todo.cursor_change_window(), // 9 is for `TAB`
+	    113 => break, // 113 is for `q`
+	    _ => {}
+	}
     }
 
     endwin();
